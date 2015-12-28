e4bf5738f6610537962e37d6db0b769ce2ea52de
Michael Wuergler <wuergler@gmail.com>
2015-12-28 12:52:55 +0000

    move tests to AVA

--- test/test.js
+++ test/test.js
@@ -2,107 +2,14 @@ import configuration from '../configure/birdwatch-config.js';
 import Birdwatch from '../dist';
 import test from 'ava';
 
-
 test('should expose a constructor', t => {
 	t.is(typeof Birdwatch, 'function');
 });
 
-<<<<<<< HEAD
-describe('Public API', function(){
-
-    it('should add a feed with .feed()', function(){
-        var birdwatch = new Birdwatch()
-            .feed('testfeed');
-        assert(birdwatch._feed[0].screenname === 'testfeed');
-    });
-
-    it('should fail when a screenname is not supplied to .feed()', function(){
-
-        var birdwatch = new Birdwatch()
-            .feed('', {});
-
-        birdwatch.start(function(err){
-                assert(err && error.message === "Screenname required");
-        });
-
-    });
-
-    it('should add a feed with options', function(){
-
-        var birdwatch = new Birdwatch()
-           .feed('testfeed', { filterTags:/test/i });
-
-        assert(birdwatch._feed[0].options.hasOwnProperty('filterTags'));
-    });
-
-    it('should fail if no feed is supplied', function(){
-
-        var birdwatch = new Birdwatch();
-        birdwatch.start(function(err){
-            assert(err.message === "You must supply at least one feed to Birdwatch");
-        });
-    });
-
-    it('should get fulfilled promise from .getCachedTweets()', function(){
-
-        var birdwatch = new Birdwatch()
-            .feed('MichaelWuergler');
-
-		birdwatch.start(function(err){});
-
-        return birdwatch.getCachedTweets().then(function(tweetdata){
-            assert(tweetdata.length > 0);
-        });
-
-    });
-
-    it('should get tweet data returned from .getCachedTweets()', function(){
-
-        var birdwatch = new Birdwatch()
-            .feed('MichaelWuergler');
-
-		birdwatch.start(function(err){
-
-		});
-
-		return birdwatch.getCachedTweets().then(function(tweetdata){
-			console.log('tweetdata created_at: ', tweetdata[0].created_at);
-			assert(tweetdata[0].created_at);
-		});
-
-    });
-
-    it('should fail when filterTags is not a valid regex', function(){
-
-        var birdwatch = new Birdwatch()
-            .feed('Twitterer', {filterTags: ''});
-
-        birdwatch.start(function(err){
-            assertEqual(err.message.slice(0,38) , "You must supply a regex to filterTags");
-        });
-
-    });
-
-    /*
-     // Currently, we can't test filterTags
-     // below test fails on iojs and node 0.12
-     // See: https://github.com/radiovisual/birdwatch/issues/4
-    it('should return only filtered tweets with option `filterTags`', function(){
-
-        var birdwatch = new Birdwatch({useTestData:true})
-            .feed('MichaelWuergler', {filterTags: /#09|#08|#07|#06/});
-
-        birdwatch.start(function(err){});
-
-        return birdwatch.getCachedTweets().then(function(tweetdata){
-            assert(tweetdata.length === 4);
-        });
-=======
 test('should add a feed with .feed()', t => {
 	const birdwatch = new Birdwatch().feed('testfeed');
 	t.is(birdwatch._feed[0].screenname, 'testfeed');
 });
->>>>>>> es6
 
 test('should fail when a screenname is not supplied to .feed()', async t => {
 	const birdwatch = new Birdwatch().feed('');
